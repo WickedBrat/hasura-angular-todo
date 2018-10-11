fb11ff8b7a5519c54e255ae4b9087172af9933c2
WickedBrat <harshsrivastav123@gmail.com>
2018-10-11 23:00:52 +0530

    updated server.js

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
+//Install express server
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/hasura-todo-app'));
+app.use(express.static('./dist/hasura-todo-app'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/hasura-todo-app/index.html'));
+res.sendFile(path.join(__dirname,'/dist/hasura-todo-app/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
